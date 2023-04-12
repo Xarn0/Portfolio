1220779dc51cbffac38d11ee7cab4e951516a547
xouli <xouli@list.ru>
2023-04-12 10:29:52 +0200

    #444

---
+++ src/components/home-project.vue
@@ -0,0 +1,31 @@
+<template lang="pug">
+include ../assets/pugmix/mixin.pug
+
+section.container
+    +CreateH("projects",true)
+    .project-list
+        +CreatCard("../static/image/craze.png","HTML SCSS Python","Myproject","описание","Live <~>",true,"Cached >=","https://xarn0.github.io/web-layout/")
+        +CreatCard("../static/image/Leo.png","HTML SCSS Python","Myproject","описание","Live <~>",false,"Cached >=","https://xarn0.github.io/web-layout-1/")
+        +CreatCard("../static/image/craze.png","HTML SCSS Python React Express Discord.js Flas Node.js Flask","Myproject","описание","Live <~>",false,"Cached >=")
+</template>
+
+
+<script>
+
+export default{
+    name:"home-project"
+}
+
+</script>
+<style scoped lang="scss">
+.project-list{
+    display: flex;
+    flex-wrap: wrap;
+    gap: 10px;
+  
+    padding-bottom: 50px;
+}
+.project__btn{
+    margin-left: 16px;
+}
+</style>
---
+++ src/components/works-components/works-apps-big.vue
@@ -0,0 +1,40 @@
+<template lang="pug">
+include ../../assets/pugmix/mixin.pug
+.conteiner.worksApps.public_margin
+    .worksApps__header 
+        +CreateH("complete-apps",false,false)
+    .worksApps__content.public_margin 
+        +CreatCard("../../static/image/work1.jpg","HTML SCSS Python","Myproject","описание","Live <~>",true,"Cached >=")
+        +CreatCard("../../static/image/work1.jpg","HTML SCSS Python","Myproject","описание","Live <~>",true,"Cached >=")
+        +CreatCard("../../static/image/work1.jpg","HTML SCSS Python Javascript php lflflf ","Myproject","описание","Live <~>",true,"Cached >=")
+        +CreatCard("../../static/image/work1.jpg","HTML SCSS Python","Myproject","описание","Live <~>",true,"Cached >=")
+</template>
+<script>
+
+export default {
+    name:"works-apps-big",
+    data(){
+        return{
+
+        }
+    }
+}
+
+</script>
+
+<style lang="scss" scoped>
+.worksApps{
+    &__content{
+        display: flex;
+        flex-wrap: wrap;
+        gap: 10px;
+        align-items: flex-start;
+    }
+    .card{
+        align-self: start;
+    }
+}
+.project__btn{
+    margin-left: 16px;
+}
+</style>
